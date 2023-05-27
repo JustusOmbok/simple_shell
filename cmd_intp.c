9a133113db93e32fb47a1f49ba7e6b58c072d9f0
Manuel <141195emmanuel@gmail.com>
2023-05-27 15:25:52 +0000

    betty styling

--- cmd_intp.c
+++ cmd_intp.c
@@ -14,27 +14,28 @@ void execute_command(char *command);
 
 /**
 * main - the entry function
-*Return: o on success
+*Return: 0 on success
 */
 
-int main() 
+int main(void)
 {
-    char input[MAX_INPUT];
+char input[MAX_INPUT];
 
-    while (1) 
-    {
-    printf(PROMPT);
-    if (fgets(input, sizeof(input), stdin) == NULL)
-        printf("\n");
-        break;
-    }
+while (1) 
+{
+printf(PROMPT);
+if (fgets(input, sizeof(input), stdin) == NULL)
+printf("\n");
+break;
+}
 
-    input[strcspn(input, "\n")] = '\0';
+input[strcspn(input, "\n")] = '\0';
 
-    if (strlen(input) > 0) {
-        execute_command(input);
-    }
-    return 0;
+if (strlen(input) > 0)
+{
+execute_command(input);
+}
+return (0);
 }
 
 /**
@@ -44,24 +45,24 @@ int main()
 */
 void execute_command(char *command) 
 {
-    pid_t pid = fork();
+pid_t pid = fork();
 
-    if (pid < 0)
-    {
-    // Error in fork()
-    perror("Fork error");
-    } else if (pid == 0)
-    {
-    // Child process
-    char *argv[] = {command, NULL};
-    if (execvp(command, argv) == -1)
-        {
-        perror("Error executing command");
-        exit(1);
-        }
-    } else
-    {
-    int status;
-    waitpid(pid, &status, 0);
-    }
+if (pid < 0)
+{
+/* Error in fork() */
+ perror("Fork error");
+} else if (pid == 0)
+{
+/* Child process */
+char *argv[] = {command, NULL};
+if (execvp(command, argv) == -1)
+{
+perror("Error executing command");
+exit(1);
+}
+} else
+{
+int status;
+waitpid(pid, &status, 0);
+}
 }
